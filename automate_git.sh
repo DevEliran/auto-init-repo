6f7315493a383b1cce915a17dec7e73b9d1cde33
Eliran Turgeman <50831652+DevEliran@users.noreply.github.com>
2020-09-28 08:42:05 +0300

    support execution from any directory

--- automate_git.sh
+++ automate_git.sh
@@ -3,5 +3,5 @@ git init
 git add -A
 git commit -m "initial commit"
 git branch -M master
-git remote add origin git@github.com:$2/$1.git
+git remote add origin git@github.com:$2/$3.git
 git push -u origin master
